70125d43c86e3c3238a619895f32de10ed59d456
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2023-01-08 14:32:43 +0900

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by 鈴木彰悟 on 2023/01/08.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
